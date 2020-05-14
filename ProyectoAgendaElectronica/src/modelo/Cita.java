1430fa2b36c9c2e82b65bc739b7c60210be7df28
KarenCh <karen6074@hotmail.com>
2020-05-14 17:15:45 -0400

    añadi la clase cita

---
+++ ProyectoAgendaElectronica/src/modelo/Cita.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Cita {
+    public String asunto;
+    public String descripcion;
+    public int horaInicio;
+    public int horaFin;
+    public int  fecha;
+    public String lugar;
+	public Cita(String asunto,String descripcion,String lugar) {
+		
+	}
+
+}
