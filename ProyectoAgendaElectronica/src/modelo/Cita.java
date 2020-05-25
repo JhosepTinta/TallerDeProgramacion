1b455b358786ed897596df927c20d2313349cd23
Jhosep <jtinta86@gmail.com>
2020-05-25 13:01:19 -0500

    Se agrego la clase Cita y Reloj.

--- ProyectoAgendaElectronica/src/modelo/Cita.java
+++ ProyectoAgendaElectronica/src/modelo/Cita.java
@@ -1,14 +1,101 @@
 package modelo;
 
-public class Cita {
-    public String asunto;
-    public String descripcion;
-    public int horaInicio;
-    public int horaFin;
-    public int  fecha;
-    public String lugar;
-	public Cita(String asunto,String descripcion,String lugar) {
-		
+public class Cita implements Comparable<Cita>{
+	
+	private String asunto;
+	private String descripcion;
+	private Reloj  horaInicio;
+	private Reloj  horaFin;
+	private Fecha  fecha;
+	private String lugar;
+	
+	public Cita(String asunto, String descripcion, Reloj horaInicio, Reloj horaFin, Fecha fecha, String lugar) {
+		this.asunto = asunto;
+		this.descripcion = descripcion;
+		this.horaInicio = horaInicio;
+		this.horaFin = horaFin;
+		this.fecha = fecha;
+		this.lugar = lugar;
 	}
 
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String asunto) {
+		this.descripcion = asunto;
+	}
+	
+	public Reloj getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(Reloj hora) {
+		this.horaInicio = hora;
+	}
+	
+	public Reloj getHoraFin() {
+		return horaFin;
+	}
+
+	public void setHoraFin(Reloj hora) {
+		this.horaFin = hora;
+	}
+
+	public Fecha getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Fecha fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(String lugar) {
+		this.lugar = lugar;
+	}
+	/**
+	 * El compareTo de una cita solo considera el asunto ni la descripcion
+	 * solo la fecha y la hora
+	 * los valores son:
+	 * 		1 la cita a comparar ocurre despues
+	 * 		0 la cita a comparar es igual
+	 * 	   -1 la cita a comparar ocurre antes
+	 * 		2 la cita a comparar choca en horarios
+	 */
+	public int compareTo(Cita otra) {
+		int res = 2;
+		if(fecha.compareTo(otra.getFecha()) == 0) {
+			if(horaInicio.compareTo(otra.getHoraInicio())==0) {
+				if(horaFin.compareTo(otra.getHoraFin())==0) {
+					res = 0;
+				}
+			}else if(horaInicio.compareTo(otra.getHoraFin())==1) {
+				res = 1;
+				
+			}else if(horaFin.compareTo(otra.getHoraInicio())==-1) {
+				res = -1;
+			}
+		}else if(fecha.compareTo(otra.getFecha())==1) {
+			res = 1;
+		}else if(fecha.compareTo(otra.getFecha())==-1) {
+			res = -1;
+		}
+		return res;
+	}
+	
+	public String toString() {
+		return asunto +"\n"+descripcion+"\n"+horaInicio+"  "+horaFin+"  "+fecha+"\n"+lugar;
+	}
 }
---
+++ ProyectoAgendaElectronica/src/modelo/Reloj.java
@@ -0,0 +1,90 @@
+package modelo;
+
+import java.time.LocalDateTime; 
+
+public class Reloj {
+	
+	private int hora;
+	private int minutos;
+	private int segundos;
+	
+	public Reloj(int hora, int minutos, int segundos) {
+		this.hora = hora;
+		this.minutos = minutos;
+		this.segundos = segundos;
+	}
+	
+	public Reloj() {
+		LocalDateTime tiempo= LocalDateTime.now();
+    	hora = tiempo.getHour();
+    	minutos = tiempo.getMinute();
+    	segundos = tiempo.getSecond();
+	}
+
+	public int getHora() {
+		return hora;
+	}
+
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+
+	public int getSegundos() {
+		return segundos;
+	}
+
+	public void setSegundos(int segundos) {
+		this.segundos = segundos;
+	}
+
+	public Reloj getHoraActual() {
+		return new Reloj();
+	}
+	
+	public int compareTo(Reloj otra) {
+		int res = 0;
+		if(hora == otra.getHora()) {
+			if(minutos == otra.getMinutos()) {
+				if(segundos == otra.getSegundos()) {
+					res = 0;
+				}else if(segundos > otra.getSegundos()) {
+					res = 1;
+				}else if(segundos < otra.getSegundos()) {
+					res = -1;
+				}
+			}else if(minutos > otra.getMinutos()) {
+				res = 1;
+			}else if(minutos < otra.getMinutos()) {
+				res = -1;
+			}
+		}else if(hora > otra.getHora()) {
+			res = 1;
+		}else if(hora < otra.getHora()) {
+			res = -1;
+		}
+		return res;
+	}
+	
+	public String toString() {
+		
+		String horaM = ""+hora;
+		String minutosM = ""+minutos;
+		String segundosM = ""+segundos;
+		
+		if(horaM.length() == 0) { horaM = "0"+hora;}
+		if(minutosM.length() == 0) { horaM = "0"+minutos;}
+		if(segundosM.length() == 0) { horaM = "0"+segundos;}
+		
+		return horaM + ":" + minutosM + ":" + segundosM;
+	}
+	
+	
+}
