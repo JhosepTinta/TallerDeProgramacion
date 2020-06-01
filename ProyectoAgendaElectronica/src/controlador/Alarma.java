311f00dd1cc71eec711e74e3d8c1f52471bcc71d
vladimirol123 <65311801+vladimirol123@users.noreply.github.com>
2020-05-31 21:39:02 -0400

    Add files via upload

---
+++ ProyectoAgendaElectronica/src/controlador/Alarma.java
@@ -0,0 +1,38 @@
+package controlador;
+
+
+
+
+import modelo.AlertaAlarma;
+import vista.PantallaAlarma;
+
+
+
+public class Alarma{
+	
+  public  PantallaAlarma pantalla;
+   
+   public Alarma() {
+	   
+	   pantalla = new PantallaAlarma();
+	   try {
+		     PantallaAlarma pantalla  =	 new PantallaAlarma();
+			pantalla.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	 }
+	
+	public static void main(String args[]) {
+		
+		new Alarma();
+          
+	}
+
+
+	
+	
+	
+	
+
+}
