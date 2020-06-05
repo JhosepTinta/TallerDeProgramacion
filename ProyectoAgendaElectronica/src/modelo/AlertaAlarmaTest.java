3e8ea5b8fe9b91e2714a5ae3c299421694d4f4d1
vladimirol123 <65311801+vladimirol123@users.noreply.github.com>
2020-06-04 21:10:36 -0400

    Add files via upload

---
+++ ProyectoAgendaElectronica/src/modelo/AlertaAlarmaTest.java
@@ -0,0 +1,16 @@
+package modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+class AlertaAlarmaTest {
+
+	@Test
+	void test() {
+		AlertaAlarma aux = new AlertaAlarma();
+		aux.CrearAlarma(9,7, 0,"PM");
+	}
+
+}
