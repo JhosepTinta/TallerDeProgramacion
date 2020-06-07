30fad00b2624b3e46e3817154b8dbc9db4b2fa58
Jhosep <jtinta86@gmail.com>
2020-06-07 13:40:09 -0500

    Se agrego el espacio para poner las pruebas JUnit

---
+++ ProyectoAgendaElectronica/src/pruebas_JUnit/CitaTest.java
@@ -0,0 +1,14 @@
+package pruebas_JUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CitaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
