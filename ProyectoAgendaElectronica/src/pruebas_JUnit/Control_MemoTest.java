ca304608fa7135def69fe3105cd442abddc7e045
Joelvellido <66577607+Joelvellido@users.noreply.github.com>
2020-06-08 10:23:06 -0400

    se agrego la estrctura de Modelo_MemoTest y Comtrol_MemoTest

---
+++ ProyectoAgendaElectronica/src/pruebas_JUnit/Control_MemoTest.java
@@ -0,0 +1,13 @@
+package Modelo;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Control_MemoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
