c10b1bc148323958a648342d2d77b359c293fb8c
Usuario <Usuario@VELLIDO>
2020-06-09 20:10:41 -0400

    aregle atributos de la clase memo

---
+++ ProyectoAgendaElectronica/src/modelo/Memo.java
@@ -0,0 +1,19 @@
+package modelo;
+
+
+
+public class Memo {
+	
+	
+	private String titulo;
+	private String texto;
+	private Fecha fechaCreacion;
+	private Reloj horaCreacion;
+	private Fecha fechamodificion;
+ 
+	public Memo() {
+		
+		
+	}
+	
+}
