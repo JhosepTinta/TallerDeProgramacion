8b1983a40d0976905d0044604e1b3e19280c8601
Jhosep <jtinta86@gmail.com>
2020-06-20 21:37:32 -0500

    Se añadio la navegacion en los paneles en la clase VistaPrincipal

--- ProyectoAgendaElectronica/src/vista/VistaPrincipal.java
+++ ProyectoAgendaElectronica/src/vista/VistaPrincipal.java
@@ -1,6 +1,8 @@
 package vista;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -9,70 +11,99 @@ import modelo.CalendarioMensual;
 import vistaAgenda.VistaAgendaPrueba;
 
 public class VistaPrincipal extends JFrame {
-	
-	
+
+	VistaAgendaPrueba a = new VistaAgendaPrueba();
+	VistaMemo b = new VistaMemo();
+	LaminaPrincipal laminaprincipal;
 
 	public VistaPrincipal() {
+		laminaprincipal = new LaminaPrincipal();
 		
-		
-		LaminaPrincipal laminaprincipal = new LaminaPrincipal();
 		setSize(950, 600);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    add(laminaprincipal);
+		add(laminaprincipal);
 	}
-    
-	private class LaminaPrincipal extends JPanel{
-    	LaminaDeBotones laminaBotones;
-    	VistaCalendarioMensual calendario;
-    	Agenda agenda ;
-    	public LaminaPrincipal() {
-    		CalendarioMensual calendarioMensual= new CalendarioMensual(5,2020);
-    		agenda = new Agenda();
-    		setLayout(new BorderLayout());
-    	    laminaBotones= new LaminaDeBotones();
-    	    calendario = new VistaCalendarioMensual(calendarioMensual,agenda);
-    		add(laminaBotones,BorderLayout.WEST);
-    		//add(calendario,BorderLayout.CENTER);
-    		add(new VistaAgendaPrueba());
-    		
-    	}
-    	
-    public class LaminaDeBotones extends JPanel{
-    	JButton inicio;
-    	JButton contactos;
-    	JButton alarma;
-    	JButton memo;
-    	JButton agenda;
-    	JButton tareasPendientes;
-    	public LaminaDeBotones() {
-    		inicio  =          new JButton("     Inicio                     ");
-    	    contactos =        new JButton("     Contactos            ");
-    	    alarma =           new JButton("     Alarma                  ");
-    	    memo =             new JButton("     Memo                    ");
-    	    agenda =           new JButton("     Agenda                 ");
-    	    tareasPendientes = new JButton("Tareas Pendientes ");
-    	    contactos.setSize(900, 30);
-    	    alarma.setSize(100, 30);
-    	    memo.setSize(100, 30);
-    	    agenda.setSize(100, 30);
-    	    tareasPendientes.setSize(100, 30);
-    		BoxLayout nuevo = new BoxLayout(this, BoxLayout.Y_AXIS);
-    		setLayout(nuevo);
-    		add(inicio);
-    		add(contactos);
-    		add(alarma);
-    		add(memo);
-    		add(agenda);
-    		add(tareasPendientes);
-    		
-    	}
-    	
-    }	
-    	
-    	
-    	
-    }
-	
+
+	private class LaminaPrincipal extends JPanel {
+		LaminaDeBotones laminaBotones;
+		JScrollPane contenedorDePaneles;
+		VistaCalendarioMensual calendario;
+		Agenda agenda;
+		
+		public LaminaPrincipal() {
+			contenedorDePaneles = new JScrollPane();
+			CalendarioMensual calendarioMensual = new CalendarioMensual(5, 2020);
+			agenda = new Agenda();
+			setLayout(new BorderLayout());
+			laminaBotones = new LaminaDeBotones();
+			calendario = new VistaCalendarioMensual(calendarioMensual, agenda);
+			add(laminaBotones, BorderLayout.WEST);
+			//add(calendario, BorderLayout.CENTER);
+			definirPanel(calendario);
+			add(contenedorDePaneles, BorderLayout.CENTER);
+			//add(a, BorderLayout.CENTER);
+			//a.setVisible(false);
+		}
+		
+		public void agregarEnPrincipal(JPanel aux) {
+			add(aux,BorderLayout.CENTER);
+		}
+		
+		public void definirPanel(JPanel panel) {
+			contenedorDePaneles.setViewportView(panel);
+		}
+
+		public class LaminaDeBotones extends JPanel {
+			JButton inicio;
+			JButton contactos;
+			JButton alarma;
+			JButton memo;
+			JButton agenda;
+			JButton tareasPendientes;
+
+			public LaminaDeBotones() {
+				inicio = new JButton("     Inicio                     ");
+				contactos = new JButton("     Contactos            ");
+				alarma = new JButton("     Alarma                  ");
+				memo = new JButton("     Memo                    ");
+				agenda = new JButton("     Agenda                 ");
+				tareasPendientes = new JButton("Tareas Pendientes ");
+				contactos.setSize(900, 30);
+				alarma.setSize(100, 30);
+				memo.setSize(100, 30);
+				agenda.setSize(100, 30);
+				tareasPendientes.setSize(100, 30);
+				BoxLayout nuevo = new BoxLayout(this, BoxLayout.Y_AXIS);
+				setLayout(nuevo);
+				add(inicio);
+				add(contactos);
+				add(alarma);
+				add(memo);
+				add(agenda);
+				add(tareasPendientes);
+
+				agenda.addActionListener(new AccionDeBoton());
+				inicio.addActionListener(new AccionDeBoton());
+				memo.addActionListener(new AccionDeBoton());
+			}
+			private class AccionDeBoton implements ActionListener{
+
+				public void actionPerformed(ActionEvent e) {
+					if(e.getSource().equals(agenda)) {
+						definirPanel(a);
+					}else if(e.getSource().equals(inicio)) {
+						definirPanel(calendario);
+					}else if(e.getSource().equals(memo)) {
+
+						definirPanel(b);
+					}
+
+				}
+				
+			}
+		}
+		
+	}
 
 }
