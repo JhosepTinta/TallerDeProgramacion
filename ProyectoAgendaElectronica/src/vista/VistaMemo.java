65a17ec12a747300d5e339783405da4e258f6aad
Jhosep <jtinta86@gmail.com>
2020-06-20 21:34:59 -0500

    Se corrigio la clase VistaMemo

---
+++ ProyectoAgendaElectronica/src/vista/VistaMemo.java
@@ -0,0 +1,18 @@
+package vista;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+
+public class VistaMemo extends JPanel {
+	
+	JButton cosa = new JButton("cosa");
+	
+	public VistaMemo() {
+		add(cosa);
+	}
+}
