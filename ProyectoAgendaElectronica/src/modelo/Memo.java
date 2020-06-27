2491db9255fa240b4dde142fe73b174305921933
Jhosep <jtinta86@gmail.com>
2020-06-27 00:50:30 -0500

    añadi la clase Memo

--- ProyectoAgendaElectronica/src/modelo/Memo.java
+++ ProyectoAgendaElectronica/src/modelo/Memo.java
@@ -1,24 +1,35 @@
 package modelo;
 
-
-
 public class Memo {
-	
-	
 	private String titulo;
 	private String texto;
 	private Fecha fechaCreacion;
 	private Reloj horaCreacion;
-	private Fecha fechaModificion;
+	private Fecha fechaModificacion;
  
-	public Memo() {
-		
-		
+	public Memo(String titulo, String texto) {
+		this.titulo = titulo;
+		this.texto = texto;
+		fechaCreacion = new Fecha();
+		fechaModificacion = new Fecha();
+		horaCreacion = new Reloj();
 	}
-	
+
 	public String getTitulo() {
-		
-		
+		return titulo;
 	}
-	
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		fechaModificacion = new Fecha();
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		fechaModificacion = new Fecha();
+		this.texto = texto;
+	}
 }
