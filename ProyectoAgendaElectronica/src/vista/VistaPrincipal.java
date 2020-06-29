494a38899d0071eecbef0a413e795c253de9321d
sebascamzam <65317770+sebascamzam@users.noreply.github.com>
2020-06-29 18:31:45 -0400

    Update VistaPrincipal.java

--- ProyectoAgendaElectronica/src/vista/VistaPrincipal.java
+++ ProyectoAgendaElectronica/src/vista/VistaPrincipal.java
@@ -58,34 +58,99 @@ public class VistaPrincipal extends JFrame {
 		scrollPaneles=new JScrollPane();
 		scrollPaneles.setBounds(175, 0, 855, 600);
 		
+		//Boton Calendario
 		btnCa = new JButton("New button");
-		btnCa.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Calendario.png")));
-		btnCa.setBounds(0, 0, 175, 100);
+		btnCa.setOpaque(false);
+		btnCa.setContentAreaFilled(false);
+		btnCa.setBorderPainted(false);
+		btnCa.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/CalendarioP.png")));
+		btnCa.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnCa.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Calendario.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnCa.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/CalendarioP.png")));//
+			}
+			
+		});
+		btnCa.setBounds(0, 0, 175, 120);
 		add(btnCa);
 		
-		btnM = new JButton("New button");
-		btnM.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Memo.png")));
-		btnM.setBounds(0, 100, 175, 100);
-		add(btnM);
-		
+		//Boton Contactos
 		btnC = new JButton("New button");
-		btnC.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Contactos.png")));
-		btnC.setBounds(0, 200, 175, 100);
+		btnC.setOpaque(false);
+		btnC.setContentAreaFilled(false);
+		btnC.setBorderPainted(false);
+		btnC.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/ContactosP.png")));		
+		btnC.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnC.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Contactos.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnC.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/ContactosP.png")));//
+			}
+			
+		});
+		btnC.setBounds(0, 120, 175, 120);
 		add(btnC);
 		
+		//Boton Agenda
 		btnAg = new JButton("New button");
+		btnAg.setOpaque(false);
+		btnAg.setContentAreaFilled(false);
+		btnAg.setBorderPainted(false);
 		btnAg.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Agenda.png")));
-		btnAg.setBounds(0, 300, 175, 100);
+		btnAg.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnAg.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Agenda.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnAg.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/AgendaP.png")));//
+			}
+			
+		});
+		btnAg.setBounds(0, 240, 175, 120);
 		add(btnAg);
 		
+		//Boton Alarma
 		btnA = new JButton("New button");
-		btnA.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Alarma.png")));
-		btnA.setBounds(0, 400, 175, 100);
+		btnA.setOpaque(false);
+		btnA.setContentAreaFilled(false);
+		btnA.setBorderPainted(false);
+		btnA.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/AlarmaP.png")));
+		btnA.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnA.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Alarma.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnA.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/AlarmaP.png")));//
+			}
+			
+		});
+		btnA.setBounds(0, 360, 175, 120);
 		add(btnA);
 	
+		//Boton Pendientes
 		btnP = new JButton("New button");
-		btnP.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Pendientes.png")));
-		btnP.setBounds(0, 500, 175, 100);
+		btnP.setOpaque(false);
+		btnP.setContentAreaFilled(false);
+		btnP.setBorderPainted(false);
+		btnP.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/PendientesP.png")));
+		btnP.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnP.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/Pendientes.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnP.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/imagenes/PendientesP.png")));//
+			}
+			
+		});
+		btnP.setBounds(0, 480, 175, 120);
 		add(btnP);
 		
 		add(scrollPaneles);
