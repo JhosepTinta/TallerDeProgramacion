f698938b341bfbd69ab5e20a4a85ecebff820c00
sebascamzam <65317770+sebascamzam@users.noreply.github.com>
2020-06-30 13:58:37 -0400

    Update Archivo.java
    Añadidos metodos paralista de contactos

--- ProyectoAgendaElectronica/src/modelo/Archivo.java
+++ ProyectoAgendaElectronica/src/modelo/Archivo.java
@@ -193,4 +193,58 @@ public class Archivo {
 		}
 		return false;
 	  }
+	
+	public int numeroC() {
+		try {
+			File file=new File("agenda.txt");
+			if(file.exists()) {
+				FileReader fr=new FileReader(file);
+				BufferedReader br = new BufferedReader(fr);
+				String linea;
+				int numero=0;
+				while((linea=br.readLine())!=null) {					
+					numero++;
+			  }
+				return numero;
+			}
+			else {
+				System.out.println("No hay nada por buscar");
+				return 0;
+				}
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		return 0;
+	  }
+	
+	public String[][] enviarMatriz(String [][]m) {
+		try {
+			File file=new File("agenda.txt");
+			if(file.exists()) {
+				FileReader fr=new FileReader(file);
+				BufferedReader br = new BufferedReader(fr);
+				String linea;
+				int numC=0;
+				while((linea=br.readLine())!=null) {
+					String contacto[]=linea.split("%");
+					Contacto c = new Contacto(contacto[0],contacto[1],contacto[2],contacto[3],contacto[4]);
+					m[numC][0]=contacto[0];
+					m[numC][1]=contacto[1];
+					m[numC][2]=contacto[2];
+					m[numC][3]=contacto[3];
+					m[numC][4]=contacto[4];
+					numC++;
+			  }
+				return m;
+			}
+			else {
+				System.out.println("No hay nada por buscar");
+				return null;
+				}
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		return null;
+	  }
+	
 	}
