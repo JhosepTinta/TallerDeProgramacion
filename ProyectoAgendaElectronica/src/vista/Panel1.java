bd57e5ff79227ec32f01036fc9210d6360564673
sebascamzam <65317770+sebascamzam@users.noreply.github.com>
2020-07-03 12:21:11 -0400

    Update Panel1.java
    uso de los botones agregado

--- ProyectoAgendaElectronica/src/vista/Panel1.java
+++ ProyectoAgendaElectronica/src/vista/Panel1.java
@@ -1,9 +1,12 @@
 package vista;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.SystemColor;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,96 +27,175 @@ public class Panel1 extends JPanel {
     
 	public Panel1() {
 		setLayout(null);
-		setBackground(Color.WHITE);
+		setBackground(SystemColor.inactiveCaptionBorder);
 		iniciarComponentes();
 	}
 	
 	private void iniciarComponentes() {
+
+		JLabel lblT = new JLabel("Buscar en Contactos");
+		lblT.setFont(new Font("Agency FB", Font.BOLD, 35));
+		lblT.setForeground(SystemColor.desktop);
+		lblT.setBounds(15, 15, 500, 30);
+		add(lblT);
+		
 		txtNB = new JTextField();
-		txtNB.setBounds(94, 25, 151, 25);
+		txtNB.setBounds(110, 80, 250, 32);
+		txtNB.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		add(txtNB);
 		txtNB.setColumns(10);
 		
 		txtAB = new JTextField();
-		txtAB.setBounds(252, 25, 151, 25);
+		txtAB.setBounds(410, 80, 250, 32);
+		txtAB.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		add(txtAB);
 		txtAB.setColumns(10);
 		
+		JLabel lblNo = new JLabel("Ingresar Nombre");
+		lblNo.setFont(new Font("Agency FB", Font.BOLD, 16));
+		lblNo.setForeground(Color.BLACK);
+		lblNo.setBounds(145, 115, 219, 30);
+		add(lblNo);
 		
-		btnBuscar = new JButton("Buscar");
-		btnBuscar.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnBuscar.setBounds(411, 24, 89, 26);
+		JLabel lblA = new JLabel("Ingresar Apellido");
+		lblA.setFont(new Font("Agency FB", Font.BOLD, 16));
+		lblA.setForeground(Color.BLACK);
+		lblA.setBounds(445, 115, 200, 30);
+		add(lblA);
+		
+		
+		btnBuscar = new JButton();
+		btnBuscar.setOpaque(false);
+		btnBuscar.setContentAreaFilled(false);
+		btnBuscar.setBorderPainted(false);
+		btnBuscar.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/botones/ContactosB1.png")));
+		btnBuscar.setBounds(700, 70, 50, 50);
+		btnBuscar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnBuscar.setIcon(new ImageIcon(Panel1.class.getResource("/botones/ContactosB2.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnBuscar.setIcon(new ImageIcon(Panel1.class.getResource("/botones/ContactosB1.png")));//
+			}
+			
+		});
 		add(btnBuscar);
 		
-		JLabel lblNewLabel = new JLabel("Buscar");
-		lblNewLabel.setFont(new Font("Agency FB", Font.PLAIN, 18));
-		lblNewLabel.setForeground(SystemColor.desktop);
-		lblNewLabel.setBounds(32, 11, 65, 22);
-		add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Contacto");
-		lblNewLabel_1.setForeground(SystemColor.desktop);
-		lblNewLabel_1.setFont(new Font("Agency FB", Font.PLAIN, 18));
-		lblNewLabel_1.setBounds(31, 35, 71, 22);
-		add(lblNewLabel_1);
-		
 		txtMosN = new JTextField();
-		txtMosN.setBounds(59, 152, 159, 27);
+		txtMosN.setBounds(130, 300, 215, 32);
+		txtMosN.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(txtMosN);
 		txtMosN.setColumns(10);
 		
 		txtMosC = new JTextField();
-		txtMosC.setBounds(228, 152, 246, 27);
+		txtMosC.setBounds(390, 300, 310, 32);
+		txtMosC.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(txtMosC);
 		txtMosC.setColumns(10);
 		
 		txtMosD = new JTextField();
-		txtMosD.setBounds(59, 210, 415, 27);
+		txtMosD.setBounds(130, 400, 570, 32);
+		txtMosD.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(txtMosD);
 		txtMosD.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Datos del contacto buscado:");
-		lblNewLabel_2.setFont(new Font("Agency FB", Font.PLAIN, 18));
-		lblNewLabel_2.setBounds(59, 99, 219, 22);
+		lblNewLabel_2.setFont(new Font("Agency FB", Font.BOLD, 29));
+		lblNewLabel_2.setForeground(new Color(51, 102, 204));
+		lblNewLabel_2.setBounds(100, 180, 350, 35);
 		add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("N\u00FAmero telf o cel:");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_3.setBounds(69, 132, 135, 14);
+		lblNewLabel_3.setFont(new Font("Agency FB", Font.BOLD, 28));
+		lblNewLabel_3.setForeground(new Color(51, 102, 204));
+		lblNewLabel_3.setBounds(135, 265, 200, 25);
 		add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("Correo electr\u00F3nico:");
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_4.setBounds(238, 132, 135, 14);
+		lblNewLabel_4.setFont(new Font("Agency FB", Font.BOLD, 28));
+		lblNewLabel_4.setForeground(new Color(51, 102, 204));
+		lblNewLabel_4.setBounds(395, 265, 300, 25);
 		add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("Direcci\u00F3n:");
-		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_5.setBounds(69, 190, 114, 14);
+		lblNewLabel_5.setFont(new Font("Agency FB", Font.BOLD, 28));
+		lblNewLabel_5.setForeground(new Color(51, 102, 204));
+		lblNewLabel_5.setBounds(135, 365, 114, 25);
 		add(lblNewLabel_5);
 		
-		btnSalir = new JButton("Lista");
-		btnSalir.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnSalir.setBounds(382, 290, 100, 30);
-		add(btnSalir);
-		
-		btnIrEditar = new JButton("Ir a Editar");
-		btnIrEditar.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnIrEditar.setBackground(SystemColor.control);
-		btnIrEditar.setForeground(new Color(0, 0, 51));
-		btnIrEditar.setBounds(160, 290, 99, 33);
-		add(btnIrEditar);
-		
-		btnCrear = new JButton("Crear Contacto");
-		btnCrear.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnCrear.setBounds(32, 290, 124, 32);
+		btnCrear = new JButton();
+		btnCrear.setOpaque(false);
+		btnCrear.setContentAreaFilled(false);
+		btnCrear.setBorderPainted(false);
+		btnCrear.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/botones/botonCrearC.png")));	
+		btnCrear.setBounds(50, 500, 130, 50);
+		btnCrear.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnCrear.setIcon(new ImageIcon(Panel1.class.getResource("/botones/botonCrearC-p.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnCrear.setIcon(new ImageIcon(Panel1.class.getResource("/botones/botonCrearC.png")));//
+			}
+			
+		});
 		add(btnCrear);
 		
-		btnEliminarC = new JButton("Eliminar");
-		btnEliminarC.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnEliminarC.setForeground(new Color(128, 0, 0));
-		btnEliminarC.setBackground(SystemColor.control);
-		btnEliminarC.setBounds(273, 290, 99, 33);
+		
+		btnIrEditar = new JButton();
+		btnIrEditar.setOpaque(false);
+		btnIrEditar.setContentAreaFilled(false);
+		btnIrEditar.setBorderPainted(false);
+		btnIrEditar.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/botones/botonIrEd.png")));	
+		btnIrEditar.setBounds(240, 500, 130, 50);
+		btnIrEditar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnIrEditar.setIcon(new ImageIcon(Panel1.class.getResource("/botones/botonIrEd-p.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnIrEditar.setIcon(new ImageIcon(Panel1.class.getResource("/botones/botonIrEd.png")));//
+			}
+			
+		});
+		add(btnIrEditar);
+		
+		btnEliminarC = new JButton();
+		btnEliminarC.setOpaque(false);
+		btnEliminarC.setContentAreaFilled(false);
+		btnEliminarC.setBorderPainted(false);
+		btnEliminarC.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/botones/botonEliminarC.png")));
+		btnEliminarC.setBounds(450, 500, 130, 50);
+		btnEliminarC.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnEliminarC.setIcon(new ImageIcon(Panel1.class.getResource("/botones/botonElimC-p.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnEliminarC.setIcon(new ImageIcon(Panel1.class.getResource("/botones/botonEliminarC.png")));//
+			}
+			
+		});
 		add(btnEliminarC);
+		
+		btnSalir = new JButton();
+		btnSalir.setOpaque(false);
+		btnSalir.setContentAreaFilled(false);
+		btnSalir.setBorderPainted(false);
+		btnSalir.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/botones/botonListarC.png")));
+		btnSalir.setBounds(640, 500, 130, 50);
+		btnSalir.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnSalir.setIcon(new ImageIcon(Panel1.class.getResource("/botones/botonListaC-p.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnSalir.setIcon(new ImageIcon(Panel1.class.getResource("/botones/botonListarC.png")));//
+			}
+			
+		});
+		add(btnSalir);
+	
 	}
 }
