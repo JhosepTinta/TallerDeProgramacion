540ddd60d3a132f9b405a880616739ec2050ccd5
sebascamzam <65317770+sebascamzam@users.noreply.github.com>
2020-07-05 10:15:12 -0400

    Update Panel3.java
    mejorada la interfaz grafica del panel 3 de contactos

--- ProyectoAgendaElectronica/src/vista/Panel3.java
+++ ProyectoAgendaElectronica/src/vista/Panel3.java
@@ -3,7 +3,9 @@ package vista;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.SystemColor;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,89 +20,131 @@ public class Panel3 extends JPanel{
 	public JTextField txtDireccion;
 	public JButton btnGuardar;
 	public JButton btnVolver;
+	
 	public Panel3() {
+		
 		setLayout(null);
-		setBackground(Color.WHITE);
+		setBackground(SystemColor.inactiveCaptionBorder);
 		iniciarComponentes();
 	}
 	private void iniciarComponentes() {
+		
+		//TITULO
+		JLabel lblNewLabel = new JLabel("Crear Contacto");
+		lblNewLabel.setForeground(SystemColor.desktop);
+		lblNewLabel.setFont(new Font("Agency FB", Font.BOLD, 35));
+		lblNewLabel.setBounds(15, 15, 500, 32);
+		add(lblNewLabel);
+		
+		//DATOS
 		txtNombre = new JTextField();
-		txtNombre.setBounds(72, 116, 186, 29);
+		txtNombre.setBounds(120, 170, 285, 35);
+		txtNombre.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(txtNombre);
 		txtNombre.setColumns(10);
-		
-		btnGuardar = new JButton("Guardar");
-		btnGuardar.setBackground(SystemColor.control);
-		btnGuardar.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnGuardar.setBounds(300,298,114,29);
-		add(btnGuardar);		
-		
+
 		txtApellido = new JTextField();
-		txtApellido.setBounds(268, 116, 186, 29);
+		txtApellido.setBounds(435, 170, 285, 35);
+		txtApellido.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(txtApellido);
 		txtApellido.setColumns(10);
 		
 		txtNumero = new JTextField();
-		txtNumero.setBounds(72, 180, 138, 29);
+		txtNumero.setBounds(120, 275, 240, 35);
+		txtNumero.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(txtNumero);
 		txtNumero.setColumns(10);
 		
 		txtCorreo = new JTextField();
-		txtCorreo.setBounds(220, 180, 234, 29);
+		txtCorreo.setBounds(390, 275, 330, 35);
+		txtCorreo.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(txtCorreo);
 		txtCorreo.setColumns(10);
 		
 		txtDireccion = new JTextField();
-		txtDireccion.setBounds(72, 241, 382, 29);
+		txtDireccion.setBounds(120, 380, 600, 35);
+		txtDireccion.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		add(txtDireccion);
 		txtDireccion.setColumns(10);
-		
-		btnVolver = new JButton("Volver");
-		btnVolver.setBackground(SystemColor.control);
-		btnVolver.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnVolver.setBounds(128, 298, 114, 26);
-		add(btnVolver);
 
-		JLabel lblNewLabel = new JLabel("A\u00F1adir nuevo contacto");
-		lblNewLabel.setForeground(SystemColor.desktop);
-		lblNewLabel.setFont(new Font("Agency FB", Font.BOLD, 26));
-		lblNewLabel.setBounds(161, 11, 245, 32);
-		add(lblNewLabel);
+		//TEXTOS
+		JLabel lblNewLabel_6 = new JLabel("Llenar los datos del contacto");
+		lblNewLabel_6.setFont(new Font("Agency FB", Font.BOLD, 29));
+		lblNewLabel_6.setForeground(new Color(51, 102, 204));
+		lblNewLabel_6.setBounds(60, 75, 350, 35);
+		add(lblNewLabel_6);
 		
-		JLabel lblNewLabel_1 = new JLabel("Direcci\u00F3n");
-		lblNewLabel_1.setForeground(Color.DARK_GRAY);
-		lblNewLabel_1.setBounds(82, 270, 84, 14);
-		add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("* Numero telf \u00F3 cel");
-		lblNewLabel_2.setForeground(Color.DARK_GRAY);
-		lblNewLabel_2.setBounds(82, 209, 128, 14);
-		add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("Correo electr\u00F3nico");
-		lblNewLabel_3.setForeground(Color.DARK_GRAY);
-		lblNewLabel_3.setBounds(230, 209, 125, 14);
-		add(lblNewLabel_3);
+		JLabel lblNewLabel_7 = new JLabel("Los datos marcados con   *   son obligatorios");
+		lblNewLabel_7.setFont(new Font("Agency FB", Font.PLAIN, 22));
+		lblNewLabel_7.setForeground(new Color(51, 102, 204));
+		lblNewLabel_7.setBounds(75, 105, 350, 35);
+		add(lblNewLabel_7);
 		
 		JLabel lblNewLabel_4 = new JLabel("* Nombre");
 		lblNewLabel_4.setForeground(Color.DARK_GRAY);
-		lblNewLabel_4.setBounds(82, 144, 114, 14);
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_4.setBounds(125, 210, 114, 17);
 		add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("Apellido");
 		lblNewLabel_5.setForeground(Color.DARK_GRAY);
-		lblNewLabel_5.setBounds(278, 144, 92, 14);
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_5.setBounds(440, 210, 92, 17);
 		add(lblNewLabel_5);
 		
-		JLabel lblNewLabel_6 = new JLabel("Llenar los datos para crear el contacto");
-		lblNewLabel_6.setFont(new Font("Agency FB", Font.PLAIN, 22));
-		lblNewLabel_6.setBounds(43, 60, 335, 26);
-		add(lblNewLabel_6);
+		JLabel lblNewLabel_2 = new JLabel("* Numero");
+		lblNewLabel_2.setForeground(Color.DARK_GRAY);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_2.setBounds(125, 315, 72, 14);
+		add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_7 = new JLabel("Los datos marcados con ( * ) son obligatorios");
-		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_7.setForeground(Color.DARK_GRAY);
-		lblNewLabel_7.setBounds(72, 82, 306, 29);
-		add(lblNewLabel_7);
+		JLabel lblNewLabel_3 = new JLabel("Correo electr\u00F3nico");
+		lblNewLabel_3.setForeground(Color.DARK_GRAY);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_3.setBounds(395, 315, 250, 14);
+		add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_1 = new JLabel("Direcci\u00F3n");
+		lblNewLabel_1.setForeground(Color.DARK_GRAY);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(125, 420, 84, 14);
+		add(lblNewLabel_1);
+		
+		//BOTONES
+		btnVolver = new JButton();
+		btnVolver.setOpaque(false);
+		btnVolver.setContentAreaFilled(false);
+		btnVolver.setBorderPainted(false);
+		btnVolver.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/botones/ContactosV1.png")));
+		btnVolver.setBounds(250, 500, 130, 50);
+		btnVolver.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnVolver.setIcon(new ImageIcon(Panel3.class.getResource("/botones/ContactosV2.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnVolver.setIcon(new ImageIcon(Panel3.class.getResource("/botones/ContactosV1.png")));//
+			}
+			
+		});
+		add(btnVolver);
+		
+		btnGuardar = new JButton();
+		btnGuardar.setOpaque(false);
+		btnGuardar.setContentAreaFilled(false);
+		btnGuardar.setBorderPainted(false);
+		btnGuardar.setIcon(new ImageIcon(VistaPrincipal.class.getResource("/botones/botonCrearC.png")));
+		btnGuardar.setBounds(450,500,130,50);
+		btnGuardar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				btnGuardar.setIcon(new ImageIcon(Panel3.class.getResource("/botones/botonCrearC-p.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				btnGuardar.setIcon(new ImageIcon(Panel3.class.getResource("/botones/botonCrearC.png")));//
+			}
+			
+		});
+		add(btnGuardar);	
 	}
 }
