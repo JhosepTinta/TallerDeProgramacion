0a6004b571411567a630e82f5b00999c147ae9e7
sebascamzam <65317770+sebascamzam@users.noreply.github.com>
2020-07-17 18:34:09 -0400

    Update TareaVista.java
    implementados los botones de tareas pendientes

--- ProyectoAgendaElectronica/src/vista/TareaVista.java
+++ ProyectoAgendaElectronica/src/vista/TareaVista.java
@@ -3,8 +3,11 @@ package vista;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -15,6 +18,8 @@ import modelo.Agenda;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import java.awt.Font;
+import java.awt.SystemColor;
+
 import javax.swing.JScrollPane;
 
 public class TareaVista extends JPanel {
@@ -25,27 +30,45 @@ public class TareaVista extends JPanel {
 	public JScrollPane scrollPane;
 	public JButton actualizar;
 	public JLabel texto;
+	public JLabel tex;
 	public JButton eliminar;
 	public JButton eliminartodo;
 	
 	public TareaVista() {
 		
-		setBackground(Color.WHITE);
+		setBackground(SystemColor.inactiveCaptionBorder);
 		setBounds(181, 0, 753, 561);
 		setLayout(null);
 		
 		list = new JList();
 		scrollPane = new JScrollPane();
-		actualizar = new JButton("ACTUALIZAR");
+		
+		actualizar = new JButton();
+		actualizar.setOpaque(false);
+		actualizar.setContentAreaFilled(false);
+		actualizar.setBorderPainted(false);
+		actualizar.setIcon(new ImageIcon(TareaVista.class.getResource("/botones/PendientesActualizar1.png")));
+		
 		texto = new JLabel("TAREAS PENDIENTES");
-		eliminar = new JButton("ELIMINAR");
-		eliminartodo = new JButton("ELIMINAR TODO");
+		tex = new JLabel("Ingresar Tarea : ");
+		tex.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		
-		texto.setFont(new Font("Arial Black", Font.PLAIN, 18));
-
+		eliminar = new JButton();
+		eliminar.setOpaque(false);
+		eliminar.setContentAreaFilled(false);
+		eliminar.setBorderPainted(false);
+		eliminar.setIcon(new ImageIcon(TareaVista.class.getResource("/botones/PendientesRealizada1.png")));
+		
+		eliminartodo = new JButton();
+		eliminartodo.setOpaque(false);
+		eliminartodo.setContentAreaFilled(false);
+		eliminartodo.setBorderPainted(false);
+		eliminartodo.setIcon(new ImageIcon(TareaVista.class.getResource("/botones/PendintesTodas1.png")));
+		
+		texto.setFont(new Font("Agency FB", Font.BOLD, 35));
 		texto1 = new JTextField();
+		texto1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		texto1.setColumns(10);
-		
 	
 		scrollPane.setViewportView(list);
 		AgregarElementos();
@@ -54,18 +77,50 @@ public class TareaVista extends JPanel {
 	
 	private void AgregarElementos()
 	{
-		scrollPane.setBounds(298, 69, 426, 424);
-		texto.setBounds(10, 11, 278, 66);
-		actualizar.setBounds(86, 126, 132, 40);
-		eliminar.setBounds(104, 231, 89, 23);
-		eliminartodo.setBounds(79, 337, 139, 23);
-		texto1.setBounds(68, 434, 188, 23);
+		scrollPane.setBounds(70, 210, 480, 320);
+		texto.setBounds(15, 15, 278, 66);
+		tex.setBounds(70, 90, 250, 35);
+		actualizar.setBounds(400, 117, 200, 50);
+		actualizar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				actualizar.setIcon(new ImageIcon(TareaVista.class.getResource("/botones/PendientesActualizar2.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				actualizar.setIcon(new ImageIcon(TareaVista.class.getResource("/botones/PendientesActualizar1.png")));//
+			}
+			
+		});
+		eliminar.setBounds(580, 300, 200, 50);
+		eliminar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				eliminar.setIcon(new ImageIcon(TareaVista.class.getResource("/botones/PendientesRealizada2.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				eliminar.setIcon(new ImageIcon(TareaVista.class.getResource("/botones/PendientesRealizada1.png")));//
+			}
+			
+		});
+		eliminartodo.setBounds(580, 400, 200, 50);
+		eliminartodo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				eliminartodo.setIcon(new ImageIcon(TareaVista.class.getResource("/botones/PendintesTodas2.png")));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				eliminartodo.setIcon(new ImageIcon(TareaVista.class.getResource("/botones/PendintesTodas1.png")));//
+			}
+			
+		});
+		texto1.setBounds(70, 120, 300, 45);
 		add(scrollPane);
 		add(texto);
 		add(actualizar);
 		add(eliminar);
 		add(eliminartodo);
 		add(texto1);
+		add(tex);
 	}
 	
 	public void setControlador(ControladorTarea entrar)
@@ -77,8 +132,6 @@ public class TareaVista extends JPanel {
 
 
 }
-
-	
 
 
 
